a5e7aaa40f2ff5b0ecb5c70f77714356e8d28f8b
Aleksandar Gajic <aleksandar.gajic@modoolar.com>
2024-02-19 12:01:51 +0100

    REVERT [16.0][FIX] auditlog: Add/Remove User Group

--- auditlog/tests/test_auditlog.py
+++ auditlog/tests/test_auditlog.py
@@ -5,7 +5,6 @@
 from odoo.tests.common import Form
 
 from odoo.addons.base.models.ir_model import MODULE_UNINSTALL_FLAG
-from odoo.addons.base.models.res_users import name_boolean_group
 
 from .common import AuditLogRuleCommon
 
